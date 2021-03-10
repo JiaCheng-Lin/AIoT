91711301dd82d887dd6f0d15498d54169c61d67d
TobyLin-exe <ttttttttoby@gmail.com>
2021-03-10 15:31:48 +0800

    20210310

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import sys
 import selenium
+import requests
 
 if __name__ == "__main__":
-    print(sys.version)
+	r = requests.get('https://cpe.cse.nsysu.edu.tw/cpe/')
+	print(r.status_code)
+
+
+	# my_params = {'key1': 'captcha'}
+	# r = requests.get('https://cpe.cse.nsysu.edu.tw/cpe/', params = my_params)
+	# print(r.url)
